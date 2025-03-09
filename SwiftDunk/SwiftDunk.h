1173b906531686e90eea61b1f2ee81c972f10c54
Stern <stern@sidestore.io>
2025-03-09 01:24:12 -0500

    Initial Commit

---
+++ SwiftDunk/SwiftDunk.h
@@ -0,0 +1,18 @@
+//
+//  SwiftDunk.h
+//  SwiftDunk
+//
+//  Created by Xavier Stern on 3/9/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftDunk.
+FOUNDATION_EXPORT double SwiftDunkVersionNumber;
+
+//! Project version string for SwiftDunk.
+FOUNDATION_EXPORT const unsigned char SwiftDunkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDunk/PublicHeader.h>
+
+
